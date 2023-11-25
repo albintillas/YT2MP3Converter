100623f2afc63eab0c374ae998691a2000c247dd
Albin Tillas <albin.tillas@hotmail.com>
2023-11-25 01:17:32 +0100

    updateTest2

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel. Yeah Baby!
+            Instantly deploy your Next.js site to a shareable URL with Vercel. Yeah Baby! Dator 2
           </p>
         </a>
       </div>
